f5d33c7b778f553b3a5382f92670967e800230ac
Gaby <rv_gaby@yahoo.com>
2019-03-07 11:32:36 +0200

    Add modules

---
+++ crawler/src/test/java/ChromeDriveProviderTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import provider.AbstractWebDriverProvider;
+import provider.ChromeDriverProvider;
+
+import static org.junit.Assert.assertTrue;
+
+public class ChromeDriveProviderTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        AbstractWebDriverProvider driverProvider = new ChromeDriverProvider();
+        String driverBinPath = "C:\\Program Files (x86)\\Google\\Chrome\\driver\\chromedriver.exe";
+        this.driver = driverProvider.getDriver(driverBinPath);
+    }
+
+    @Test
+    public void testGetDriver() {
+        assertTrue(driver.toString().matches("ChromeDriver: chrome on [^(]*\\(\\w+\\)"));
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
